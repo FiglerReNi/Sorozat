e8793de22df460ffabe692508a31f80b685931bb
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2020-04-09 20:47:22 +0200

    Add files via upload
    forras

---
+++ src/Csokkeno.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FReni
+ */
+public class Csokkeno {
+    public static void main(String[]args){
+        for(int i=20; i>0; i--){
+            System.out.print(i*i+" ");
+       }
+    }
+    }
---
+++ src/Novekvo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FReni
+ */
+public class Novekvo {
+    public static void main(String[]args){
+        int a=5;
+        while(a<=100){
+            System.out.print(a+" ");
+            a=a+5;
+        }
+    }    
+}
